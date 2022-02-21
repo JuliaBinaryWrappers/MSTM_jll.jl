f0d788f20fce475bad6ab59859f217f2e0022c7d
jlbuild <juliabuildbot@gmail.com>
2022-02-21 11:52:16 +0000

    MSTM_jll build 4.0.0+0

---
+++ src/MSTM_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule MSTM_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("MSTM")
+JLLWrappers.@generate_main_file("MSTM", UUID("2c0bff41-3d2c-5a3a-b138-70c96ef824a5"))
+end  # module MSTM_jll
